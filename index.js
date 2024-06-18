4ef4d5a977fc3417c77ec4079112a3d0d35a8466
HerbertAMS <herbert.ams@gmail.com>
2024-06-17 23:01:44 -0300

    projeto concluido

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-console.log("Hello word!")
+/* // Utilizando if/ else if
+
+let nomePersonagem = "Naskar";
+let nivel = 900;
+let nomeNivel;
+
+if (nivel <= 1000) {
+  nomeNivel = "Ferro";
+} else if (nivel <= 1000) {
+  nomeNivel = "Bronze";
+} else if (nivel <= 2000) {
+  nomeNivel = "Prata";
+} else if (nivel <= 5000) {
+  nomeNivel = "Ouro";
+} else if (nivel <= 7000) {
+  nomeNivel = "Platina";
+} else if (nivel <= 9000) {
+  console.log(
+    nomeNivel = "Ascendente"
+  );
+} else if (nivel <= 10000) {
+  nomeNivel = "Imortal";
+} else {
+  nomeNivel = "Radiante";
+}
+
+
+console.log(`O Heroi ${nomePersonagem} está no nível de ${nomeNivel}`);  */
+
+
+// Utilizando Switch case
+const nomePersonagem = "Herbert";
+const nivel = 8010;
+let nomeNivel;
+
+switch (true) {
+  case nivel <= 1000:
+    nomeNivel = "Ferro";
+    break;
+  case nivel <= 2000:
+    nomeNivel = "Bronze";
+    break;
+  case nivel <= 5000:
+    nomeNivel = "Prata";
+    break;
+  case nivel <= 7000:
+    nomeNivel = "Ouro";
+    break;
+  case nivel <= 8000:
+    nomeNivel = "Platina";
+    break;
+  case nivel <= 9000:
+    nomeNivel = "Ascendente";
+    break;
+  case nivel <= 10000:
+    nomeNivel = "Imortal";
+    break;
+  default:
+    nomeNivel = "Radiante";
+}
+
+console.log(`O Heroi ${nomePersonagem} está no nível de ${nomeNivel}`); 
+
+
